b0ccc282f31b516db2234bb1394f80ceb05d6263
tkhwang <tkhwang@gmail.com>
2018-08-13 16:33:48 +0900

    [+] accounts analysis added.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 My own understanding and summary of [ethereum/go-ethereum: Official Go implementation of the Ethereum protocol](https://github.com/ethereum/go-ethereum) inspired by [NAKsir-melody/go-ethereum: Translate code comments into korean (한국어 주석번역 프로젝트)](https://github.com/NAKsir-melody/go-ethereum).
 
 For my own understanding the ethereum, I will briefly summarize it as I read the code.
+
+- UML diagram
+- Summary by myself
+- ...
+
+## Modules
+
+### Accounts
+
+![accounts.go](./accounts/accounts.png)
--- accounts/accounts.go
+++ accounts/accounts.go
@@ -28,6 +28,7 @@ import (
 
 // Account represents an Ethereum account located at a specific location defined
 // by the optional URL field.
+// Account 구조체는 이더리움 계정을 나타내는데, 이는 옵션으로 URL field로 정의된 특정 위치에 위치할 수 있다.
 type Account struct {
 	Address common.Address `json:"address"` // Ethereum account address derived from the key
 	URL     URL            `json:"url"`     // Optional resource locator within a backend
@@ -35,6 +36,8 @@ type Account struct {
 
 // Wallet represents a software or hardware wallet that might contain one or more
 // accounts (derived from the same seed).
+// Wallet 인터페이스는 동일한 seed로부터 유도된 하나 혹은 그 이상의 계정을 포함하는 소프트웨어
+// 혹은 하드웨어 wallet을 나타낸다.
 type Wallet interface {
 	// URL retrieves the canonical path under which this wallet is reachable. It is
 	// user by upper layers to define a sorting order over all wallets from multiple
@@ -44,54 +47,82 @@ type Wallet interface {
 	// Status returns a textual status to aid the user in the current state of the
 	// wallet. It also returns an error indicating any failure the wallet might have
 	// encountered.
+	// Status 메서드는 현재 wallet의 상태를 유저에게 textual status를 반환한다.
+	// 또한, wallet이 이전에 발생한 error들을 반환하여 준다.
 	Status() (string, error)
 
 	// Open initializes access to a wallet instance. It is not meant to unlock or
 	// decrypt account keys, rather simply to establish a connection to hardware
 	// wallets and/or to access derivation seeds.
-	//
+
+	// Open 메서드는 wallet 지갑 객체에 접근하기 위한  초기화를 진행한다. 실제로 언락 혹은
+	// 어카운트 키를 복호화하는 것이 아니라 단순히 하드웨어 지갑에 대한 연결을 설정하거나
+	// 시드에에 접근하는 것과 같은 단순한 작업이다.
+
 	// The passphrase parameter may or may not be used by the implementation of a
 	// particular wallet instance. The reason there is no passwordless open method
 	// is to strive towards a uniform wallet handling, oblivious to the different
 	// backend providers.
-	//
+
+	// Passphrase 파라미터는 특정한 지갑의 경우에는 사용되지 않을 수도 있다.
+	// Passphrase를 넣지 않는 open 메서드가 없는 이유는 다른 백엔드 제공자와 무관하게
+	// 지갑 관련하여 공통화된 방법을 제공하기 위함입니다.
+
 	// Please note, if you open a wallet, you must close it to release any allocated
 	// resources (especially important when working with hardware wallets).
+
+	// 만약 지갑을 open 했으면 (특히 하드웨어 지갑과 연동했을 때에는 더욱 중요한데) 리소스 해제를
+	// 위하여 반드시 close 해야한다.
+
 	Open(passphrase string) error
 
 	// Close releases any resources held by an open wallet instance.
+	// Open 지갑 객체에 의하여 잡혀진 모든 리소스를 해제한다.
 	Close() error
 
 	// Accounts retrieves the list of signing accounts the wallet is currently aware
 	// of. For hierarchical deterministic wallets, the list will not be exhaustive,
 	// rather only contain the accounts explicitly pinned during account derivation.
+
+	// 계정 함수는 현재 지갑이 알고 있는 signing accounts의 리스트를 반환한다.
+	// HD (계층적으로 주소를 생성하는) 지갑의 경우에는
 	Accounts() []Account
 
 	// Contains returns whether an account is part of this particular wallet or not.
+	// Contains 메소드는 파라미터인 account가 해당 특정 지갑의 일부인지 아닌지를 반환해준다.
 	Contains(account Account) bool
 
 	// Derive attempts to explicitly derive a hierarchical deterministic account at
 	// the specified derivation path. If requested, the derived account will be added
 	// to the wallet's tracked account list.
+
+	// Devier 메소드는 특정한 derivation path에 대해서 계층 유도 계정을 유도한다.
+	// 요청이 주어지면 유도 계정은 지갑의 계정 리스트에 추가된다.
 	Derive(path DerivationPath, pin bool) (Account, error)
 
 	// SelfDerive sets a base account derivation path from which the wallet attempts
 	// to discover non zero accounts and automatically add them to list of tracked
 	// accounts.
-	//
+
+	// SelfDrive 메소드는 지갑이 non zero 계정을 찾을려고 하는 지갑 기본 유도 패스의 기본을
+	// 설정하며 이는 자동으로 계정 리스트에 추가된다.
+
 	// Note, self derivaton will increment the last component of the specified path
 	// opposed to decending into a child path to allow discovering accounts starting
 	// from non zero components.
-	//
+
 	// You can disable automatic account discovery by calling SelfDerive with a nil
 	// chain state reader.
 	SelfDerive(base DerivationPath, chain ethereum.ChainStateReader)
 
 	// SignHash requests the wallet to sign the given hash.
-	//
+	// SignHash 메소드는 지갑에서 주어진 Hash에 대해서 서명하도록 요청한다.
+
 	// It looks up the account specified either solely via its address contained within,
 	// or optionally with the aid of any location metadata from the embedded URL field.
+
 	//
+
 	// If the wallet requires additional authentication to sign the request (e.g.
 	// a password to decrypt the account, or a PIN code o verify the transaction),
 	// an AuthNeededError instance will be returned, containing infos for the user
@@ -101,7 +132,8 @@ type Wallet interface {
 	SignHash(account Account, hash []byte) ([]byte, error)
 
 	// SignTx requests the wallet to sign the given transaction.
-	//
+	// SignTx 메소드는 지갑에 주어진 트랜잭션에 서명하도록
+
 	// It looks up the account specified either solely via its address contained within,
 	// or optionally with the aid of any location metadata from the embedded URL field.
 	//
